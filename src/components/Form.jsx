fa7232db57838c475bed649d443fc061535da367
martinoremonje <m.monjeorellana@gmail.com>
2024-11-08 00:02:32 -0300

    agregado un boton add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,28 @@
-import React from 'react';
-
 const Form = ({ handleForm }) => {
-    // Forzar el enfoque en el campo de entrada después de seleccionar una opción
-    const inputRef = React.useRef(null);
-
-    const handleCategoryChange = () => {
-        inputRef.current.focus();
-    };
-
     return (
         <form onSubmit={handleForm} className="flex rounded-md bg-white overflow-hidden py-3 gap-4 items-center px-4">
-            <span className="border-gray-300 h-5 w-5 inline-block rounded-full border-2"></span>
+            <button 
+                type="submit"  // Cambiar a "submit" para que envíe el formulario
+                className="px-2 py-1 rounded bg-blue-500 text-white"
+            >
+                Add
+            </button>
             <input 
                 name="todo" 
                 type="text" 
                 placeholder="Crea una tarea" 
-                className="w-full text-gray-400 outline-none"
-                ref={inputRef}
+                className="w-full text-gray-400 outline-none" 
             />
-            <select 
-                name="category" 
-                className="text-gray-400 border-none bg-transparent outline-none"
-                onChange={handleCategoryChange}
-            >
+            <select name="category" className="text-gray-400 border-none bg-transparent outline-none">
                 <option value="home">Home</option>
                 <option value="work">Work</option>
             </select>
-            <button type="submit" className="hidden">Submit</button> {/* Este botón permite enviar con Enter en algunos móviles */}
         </form>
     );
 };
 
 export default Form;
+
+
+
+
