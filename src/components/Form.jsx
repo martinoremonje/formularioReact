d981a3f2d30e9832f8febe7b8703cea9fe20e2b1
martinoremonje <m.monjeorellana@gmail.com>
2024-11-12 16:22:33 -0300

    transitions 3.0

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 const Form = ({ handleForm, fill }) => {
     return (
-        <form onSubmit={handleForm} className={`flex rounded-md bg-white overflow-hidden py-3 gap-4 items-center px-4 transition-all duration-1000 ${fill !== "#000" ? "bg-gray-800 text-white " : "text-gray-600"}`}>
+        <form onSubmit={handleForm} className={`flex rounded-md  overflow-hidden py-3 gap-4 items-center px-4 transition-all duration-1000 ${fill !== "#000" ? "bg-gray-800 text-white " : "text-gray-600 bg-white"}`}>
             <button 
                 type="submit"  
                 className="px-2 py-1 rounded bg-blue-500 text-white"
--- src/components/TodoComputed.jsx
+++ src/components/TodoComputed.jsx
@@ -1,6 +1,6 @@
 const TodoComputed = ({todos, setTodos, fill}) => { 
     return (
-    <section className={`flex justify-between px-4 py-4 bg-white rounded-b-md transition-all duration-1000 ${fill !== "#000" ? "bg-gray-800 text-white" : "text-gray-600"}`}>
+    <section className={`flex justify-between px-4 py-4  rounded-b-md transition-all duration-1000 ${fill !== "#000" ? "bg-gray-800 text-white" : "text-gray-600 bg-white"}`}>
       <span className="text-gray-400">{todos.filter(todo => !todo.completed).length} Tareas Activas </span>
       <button onClick={()=>{todos.length ? setTodos(todos.map(todo => todo.completed ? {...todo, completed: false} : todo)) : alert("add a Todo washin")}} className="text-gray-500 hover:text-blue-500">Descompletar all</button>
     </section>
